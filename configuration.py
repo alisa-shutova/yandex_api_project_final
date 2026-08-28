51d00673b26e33ae5b4c8c01669d5a99fff8c39e
alisa-shutova <morbid.angel.al@gmail.com>
2026-08-28 16:47:53 +0400

    Fix project after review

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://3e6e977b-a0fa-473d-befb-fe0917537484.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://1a8e919e-4a84-457b-8b0b-dc870b9fc658.serverhub.praktikum-services.ru/"
 DOC_PATH = "/docs/"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
